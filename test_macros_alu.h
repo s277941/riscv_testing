5eef669c9e297723a1dec39768e2a5baf59ac708
ANDREA RUSSO s277941 <s277941@studenti.polito.it>
2021-01-06 11:34:21 +0100

    .

--- test_macros_alu.h
+++ test_macros_alu.h
@@ -138,352 +138,352 @@ TEST_CLIP();\
 
 #define TEST_ALU() \
 p.extractu x3, x1, 31, 0;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.extractr x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.extractur x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.insertr x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.bclrr x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.bsetr x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.abs x3, x1;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.slet x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.sletu x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.min x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.minu x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.max x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.maxu x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.exths x3, x1;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.exthz x3, x1;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.extbs x3, x1;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.extbz x3, x1;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.clip x3, x1, 1;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.clipu x3, x1, 2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.clipr x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.clipur x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.addN x3, x1, x2, 0;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.adduN x3, x1, x2, 0;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.addRN x3, x1, x2, 0;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.adduRN x3, x1, x2, 0;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.subN x3, x1, x2, 0;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.subuN x3, x1, x2, 0;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.subRN x3, x1, x2, 0;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.subuRN x3, x1, x2, 0;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.addNr x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.adduNr x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.addRNr x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.adduRNr x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.subNr x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.subuNr x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.subRNr x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.subuRNr x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.add.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.add.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.add.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.add.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.sub.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.sub.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.sub.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.sub.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.avg.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.avg.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.avg.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.avg.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.avgu.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.avgu.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.avgu.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.avgu.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.min.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.min.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.min.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.min.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.minu.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.minu.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.minu.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.minu.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.max.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.max.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.max.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.max.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.maxu.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.maxu.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.maxu.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.maxu.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.srl.sci.h x3, x1, 31;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.srl.sci.b x3, x1, 31;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.sra.sci.h x3, x1, 31;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.sra.sci.b x3, x1, 31;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.sll.sci.h x3, x1, 31;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.sll.sci.b x3, x1, 31;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.or.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.or.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.or.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.or.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.xor.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.xor.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.xor.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.xor.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.and.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.and.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.and.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.and.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.abs.h x3, x1;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.abs.b x3, x1;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.shuffle.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.shuffle.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.extract.h x3, x1, 0;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.extract.b x3, x1, 1;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.extractu.h x3, x1, 1;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.extractu.b x3, x1, 2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.shuffle.sci.h x3, x1, 31;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.shuffleI0.sci.b x3, x1, 31;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.shuffleI1.sci.b x3, x1, 31;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.shuffleI2.sci.b x3, x1, 31;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.shuffleI3.sci.b x3, x1, 31;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.shuffle2.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.shuffle2.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 \
 pv.shuffle.h x3, x1, x3;\
 pv.shuffle.b x3, x3, x2;\
 pv.shuffle.sci.h x3, x3, 1;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.shuffle.sci.h x3, x3, 2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.shuffle.sci.h x3, x3, 3;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.shuffleI0.sci.b x3, x3, 21;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.shuffleI1.sci.b x3, x3, 10;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.shuffleI2.sci.b x3, x3, 21;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.shuffleI3.sci.b x3, x3, 10;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.shuffle2.h x3, x3, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.shuffle2.b x3, x1, x3;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.extract.h x3, x3, 0;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.extract.b x3, x3, 1;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.extractu.h x3, x3, 1;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.extractu.b x3, x3, 2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.pack.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.packhi.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.packlo.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpeq.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpeq.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpeq.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpeq.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpne.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpne.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpne.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpne.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpgt.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpgt.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpgt.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpgt.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpge.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpge.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpge.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpge.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmplt.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmplt.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmplt.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmplt.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmple.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmple.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmple.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmple.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpgtu.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpgtu.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpgtu.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpgtu.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpgeu.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpgeu.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpgeu.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpgeu.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpltu.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpltu.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpltu.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpltu.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpleu.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpleu.sc.h x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpleu.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 pv.cmpleu.sc.b x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 divu x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.adduN x3, x1, x2, 0;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 p.subN x3, x1, x2, 0;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 div x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 REM x3,x1,x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 REMU x3,x1,x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 srl x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 sll x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 sra x3, x1, x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 SLT x3,x1,x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 SLTU x3,x1,x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 XOR x3,x1,x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 OR x3,x1,x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 AND x3,x1,x2;\
-sw  x3, 0(x31);\
+sw  x3, 8(x31);\
 
 #define TEST_CLIP()\
 li x1, 0xFFFF;\
@@ -553,134 +553,134 @@ sw x3, 8(x31);\
         li  x1, val1; \
         li  x2, val2; \
       	inst x3, x1, x2, 1; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 2; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 3; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 4; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
 	inst x3, x1, x2, 5; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 6; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 7; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 8; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
 	inst x3, x1, x2, 9; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 10; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 11; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 12; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
 	inst x3, x1, x2, 13; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 14; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 15; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 16; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
 	inst x3, x1, x2, 17; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 18; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 19; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 20; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
 	inst x3, x1, x2, 21; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 22; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 23; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 24; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
 	inst x3, x1, x2, 25; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 26; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 27; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 28; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
 	inst x3, x1, x2, 29; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 30; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, x2, 31; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
 
     #define TEST_SHF2(inst, val1) \
         li  x1, val1; \
       	inst x3, x1, 0; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 1; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 2; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 3; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 4; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
 	inst x3, x1, 5; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 6; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 7; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 8; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
 	inst x3, x1, 9; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 10; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 11; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 12; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
 	inst x3, x1, 13; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 14; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 15; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 16; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
 	inst x3, x1, 17; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 18; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 19; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 20; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
 	inst x3, x1, 21; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 22; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 23; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 24; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
 	inst x3, x1, 25; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 26; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 27; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 28; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
 	inst x3, x1, 29; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 30; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
       	inst x3, x1, 31; \
-	sw  x3, 1(x31); \
+	sw  x3, 4(x31); \
 
 
 #define TEST_MANIP(val) \
